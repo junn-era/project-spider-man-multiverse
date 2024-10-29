91a393c3cf3a3ead0ce3c0974a1d55d075fd2535
Junn-era <junnera@gmail.com>
2024-10-29 14:08:56 -0300

    Project finished

--- assets/scripts/script2.js
+++ assets/scripts/script2.js
@@ -1,5 +1,4 @@
 function handleMouseEnter() {
-  //console.log('handle mouse enter foi chamado', this)
   this.classList.add('s-card--hovered');
   document.body.id = `${this.id}-hovered`;
 }
@@ -10,16 +9,16 @@ function handleMouseLeave() {
 }
 
 function addEventListenersToCards() {
-  const cardsElements = document.getElementsByClassName('s-card');
-
-  for (let index = 0; index < cardsElements.length; index++) {
-    const card = cardsElements[index];
+  const cardElements = document.getElementsByClassName('s-card');
+  
+  for (let index = 0; index < cardElements.length; index++) {
+    const card = cardElements[index];
     card.addEventListener('mouseenter', handleMouseEnter);
     card.addEventListener('mouseleave', handleMouseLeave);
   }
 }
 
-document.addEventListener('DOMContentLoaded', addEventListenersToCards, false);
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
 
 function selectCarouselItem(selectedButtonElement) {
   const selectedItem = selectedButtonElement.id;
